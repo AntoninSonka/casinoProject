6d3732b62fedfde2ee6224ec6633c84776466b39
AntoninSonka <antonin.sonka@outlook.com>
2025-03-04 14:52:30 +0100

    connector

--- backend/server/php/connection.php
+++ backend/server/php/connection.php
@@ -1,14 +1,56 @@
 <?php
 
-$servername = "192.168.50.100:3306";
-$username = "antonin";
-$password = "root";
+class Database {
+    public function __construct($host, $user, $pass, $db){
+        try {
+            $this->conn = new mysqli($host, $user, $pass, $db);
+            if($this->conn->connect_error){
+                throw new Exception("Chyba tvorby conn " . $this->conn->connect_error);
+            }
+            else{
+                echo "conn succ\n";
+            }
+        }
+        catch(Exception $e){
+            die("Error code: " . $e->getMessage());
+        }//konec catch
+    }//konec constructoru
 
-$conn = new mysqli($servername, $username, $password);
+    public function query($stmt){
+        try {
+            $result = $this->conn->query($stmt);
 
-if($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
+            if(!$result){
+                throw new Exception("Chyba vytvoreni vysledku pro dotaz na DB " . $this->conn->error);
+                
+            }
+            return $result;
+        }
+        catch (Exception $e){
+            echo "Error code: " . $e->getMessage();
+        }
+    }
+    public function __destruct(){
+        if($this->conn){
+            $this->conn->close();
+        }
+    }
+    private $conn;
+}//konec db class
+
+try{
+    $db = new Database("127.0.0.1:3306", "antoninsonka", "JoHingo6Oo", "antoninsonka");
+    $result = $db->query("select * from user");
+    if($result && $result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            echo "User: " . $row["username"];
+            //echo "<br>";
+            echo "\n";
+        }
+    }
 }
-echo "Connection estabilished";
-
+catch(Exception $e){
+    die("Error code " . $e->getMessage());
+}
+die();
 ?>
